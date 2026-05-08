e726db711fcef9aa1563e3f35fa80fd152b54298
Harshit Goyal <18harshit052005@gmail.com>
2026-05-08 23:35:28 +0530

    fixing cors for frontend

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:import.meta.env.VITE_API_URL,
-    timeout:10000, //10s
+    baseURL: import.meta.env.VITE_API_URL,
+    timeout: 10000,
+    withCredentials: true
 })
-
 // Response interceptor
 axiosInstance.interceptors.response.use(
     (response) => {
